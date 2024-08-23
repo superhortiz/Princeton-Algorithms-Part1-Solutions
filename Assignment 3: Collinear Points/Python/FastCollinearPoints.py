b8eaa4a279192581cd0411856908ea0a949fd685
Hector Ortiz <superhortiz@gmail.com>
2024-08-23 12:13:52 +0300

    Update FastCollinearPoints.py

--- Assignment 3: Collinear Points/Python/FastCollinearPoints.py
+++ Assignment 3: Collinear Points/Python/FastCollinearPoints.py
@@ -3,50 +3,47 @@ from LineSegment import LineSegment
 from copy import deepcopy
 
 class FastCollinearPoints:
-	def __init__(self, points):
-	"""
-	Initializes a FastCollinearPoints object with an array of points.
+    def __init__(self, points):
+    """
+    Initializes a FastCollinearPoints object with an array of points.
 
-	Args:
-		points (list[Point]): An array of points.
-	Raises:
-		ValueError: If the input array or any point in it is None.
-		ValueError: If any two points in the array are the same.
-        """
-		if points is None:
-			raise ValueError()
+    Args:
+        points (list[Point]): An array of points.
+    Raises:
+        ValueError: If the input array or any point in it is None.
+        ValueError: If any two points in the array are the same.
+    """
+        if points is None:
+            raise ValueError()
 
-		n = len(points)
-		clone = []
-		self.segments = []
+        n = len(points)
+        clone = []
+        self.segments = []
 		
-		for i in range(n):
-			if points[i] is None:
-				raise ValueError()
-			else:
-				for j in range(i + 1, n):
-					if points[j] is None and points[i] == points[j]:
-						raise ValueError()
-				clone.append(points[i])
+        for i in range(n):
+            if points[i] is None:
+                raise ValueError()
+            else:
+                for j in range(i + 1, n):
+                    if points[j] is None and points[i] == points[j]:
+                        raise ValueError()
+                clone.append(points[i])
 
-		clone.sort()	# Sorting the points is crucial; it allows efficient segment definition
-				# Once sorted, we can identify line segments efficiently when encountered for the first time
-				# This approach helps us avoid storing duplicate segments
+        clone.sort()	# Sorting the points is crucial; it allows efficient segment definition
+                        # Once sorted, we can identify line segments efficiently when encountered for the first time
+                        # This approach helps us avoid storing duplicate segments
 
-		for p in range(n):
-			copy = deepcopy(clone)
-			copy.sort(key = points[p].slopeOrder())
-
-			q = 1
-			while q < n:
-				start = q
-				while q < n and points[p].slopeTo(copy[q - 1]) == points[p].slopeTo(copy[q]):
-					q += 1
-
-				if q - start >= 2 and points[p] < copy[start - 1]:
-					self.segments.append(LineSegment(points[p], copy[q - 1]))
-
-				q += 1
+        for p in range(n):
+            copy = deepcopy(clone)
+            copy.sort(key = points[p].slopeOrder())
+            q = 1
+            while q < n:
+                start = q
+                while q < n and points[p].slopeTo(copy[q - 1]) == points[p].slopeTo(copy[q]):
+                    q += 1
+                if q - start >= 2 and points[p] < copy[start - 1]:
+                    self.segments.append(LineSegment(points[p], copy[q - 1]))
+                q += 1
 
     def number_of_segments(self):
         """
